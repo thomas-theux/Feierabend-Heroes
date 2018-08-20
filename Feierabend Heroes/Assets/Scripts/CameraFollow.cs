1876f50c447d0ebf69f0093b3661be76d9671c26
Thomas Theux <t.grikschas@gmail.com>
2018-08-20 21:49:59 +0200

    Added camera spawning for up to 4 players

---
+++ Feierabend Heroes/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public int cameraID = 0;
+
+	private GameObject characterModel;
+	private Vector3 offset;
+	private int camIdentifier;
+
+	private Camera cam;
+
+	void Start ()
+	{
+		characterModel = GameObject.Find("Character0" + (cameraID / 10) + ("(Clone)"));
+		offset = new Vector3(0f, 14f, -10f);
+		transform.Rotate(60f, 0, 0);
+
+		camIdentifier = CharacterStats.playerCount + cameraID;
+		cam = GetComponent<Camera>();
+
+		switch (camIdentifier) {
+			case 11:
+				// 1 Spieler – Position 1
+				cam.rect = new Rect(0, 0, 1, 1);
+				break;
+			case 12:
+				// 2 Spieler – Position 1
+				cam.rect = new Rect(0, 0, 0.5f, 1);
+				break;
+			case 13:
+				// 3 Spieler – Position 1
+				cam.rect = new Rect(0.25f, 0.5f, 0.5f, 0.5f);
+				break;
+			case 14:
+				// 4 Spieler – Position 1
+				cam.rect = new Rect(0, 0.5f, 0.5f, 0.5f);
+				break;
+
+			case 22:
+				// 2 Spieler – Position 2
+				cam.rect = new Rect(0.5f, 0, 0.5f, 1);
+				break;
+			case 23:
+				// 3 Spieler – Position 2
+				cam.rect = new Rect(0, 0, 0.5f, 0.5f);
+				break;
+			case 24:
+				// 4 Spieler – Position 2
+				cam.rect = new Rect(0.5f, 0.5f, 0.5f, 0.5f);
+				break;
+
+			case 33:
+				// 3 Spieler – Position 3
+				cam.rect = new Rect(0.5f, 0, 0.5f, 0.5f);
+				break;
+			case 34:
+				// 4 Spieler – Position 3
+				cam.rect = new Rect(0, 0, 0.5f, 0.5f);
+				break;
+
+			case 44:
+				// 4 Spieler – Position 4
+				cam.rect = new Rect(0.5f, 0, 0.5f, 0.5f);
+				break;
+		}
+	}
+
+	void Update () {
+		transform.position = characterModel.transform.position + offset;
+	}
+
+}
--- Feierabend Heroes/Assets/Scripts/CharacterStats.cs
+++ Feierabend Heroes/Assets/Scripts/CharacterStats.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class CharacterStats : MonoBehaviour {
 
-	public static int playerCount = 1;
+	public static int playerCount = 4;
 	public GameObject characterModel;
+	public GameObject cameraFollow;
 
 	public static float[] characterSpeed = {0f, 10f, 10f, 10f, 10f};
 	public static float[] characterJumpHeight = {0f, 2f, 2f, 2f, 2f};
@@ -14,9 +15,15 @@ public class CharacterStats : MonoBehaviour {
 	void Start ()
 	{
 		for (int i = 1; i < playerCount + 1; i++) {
+			// Instantiate a character, rename it and give it an ID
 			characterModel.transform.name = "Character0" + i;
 			characterModel.GetComponent<CharacterMovement>().charID = i;
 			Instantiate(characterModel, new Vector3(i * 2, 1.5f, 0), Quaternion.identity);
+
+			// Instantiate the according Camera, rename it and give it an ID
+			cameraFollow.transform.name = "Camera0" + i;
+			cameraFollow.GetComponent<CameraFollow>().cameraID = i * 10;
+			Instantiate(cameraFollow, new Vector3(0, 0, 0), Quaternion.identity);
 		}	
 	}
 
