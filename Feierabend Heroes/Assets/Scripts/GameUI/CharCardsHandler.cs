0cb0bdbed9c3fe9474f023105f03ae4a016745d8
Thomas Theux <t.grikschas@gmail.com>
2019-01-30 13:23:27 +0100

    Implemented all cards states

--- Feierabend Heroes/Assets/Scripts/GameUI/CharCardsHandler.cs
+++ Feierabend Heroes/Assets/Scripts/GameUI/CharCardsHandler.cs
@@ -14,6 +14,7 @@ public class CharCardsHandler : MonoBehaviour {
 	public GameObject pressToJoinGO;
 	public GameObject charClassGO;
 	public GameObject arrowNavGO;
+	public GameObject readyGO;
 
 	// REWIRED
 	private bool selectButton;
@@ -36,20 +37,26 @@ public class CharCardsHandler : MonoBehaviour {
 	private void CheckForGamepad() {
 		if (GameUIHandler.connectedGamepads >= charID+1 && !isConnected) {
 			currentStatus = 1;
-			isConnected = true;
 			DisplayCurrentStatus();
 		} else if (GameUIHandler.connectedGamepads < charID+1 && isConnected) {
 			currentStatus = 0;
-			isConnected = false;
 			DisplayCurrentStatus();
 		}
 	}
 
 
 	private void CheckForInput() {
-		if (currentStatus == 1) {
+		if (currentStatus == 1 || currentStatus == 2) {
 			if (selectButton) {
-				currentStatus = 2;
+				currentStatus++;
+				DisplayCurrentStatus();
+			}
+		}
+
+		if (currentStatus == 2 || currentStatus == 3) {
+			if (cancelButton) {
+				currentStatus--;
+				DisplayCurrentStatus();
 			}
 		}
 	}
@@ -58,24 +65,36 @@ public class CharCardsHandler : MonoBehaviour {
 	private void DisplayCurrentStatus() {
 		switch(currentStatus) {
 			case 0:
+				isConnected = false;
 				gamepadIcon.color = new Color32(255, 255, 255, 50);
 				pressToJoinGO.SetActive(false);
-				break;
-			case 1:
-				gamepadIcon.color = new Color32(255, 255, 255, 255);
-				pressToJoinGO.SetActive(true);
-
 				charClassGO.SetActive(false);
 				arrowNavGO.SetActive(false);
+				readyGO.SetActive(false);
+				break;
+			case 1:
+				isConnected = true;
+				gamepadIcon.color = new Color32(255, 255, 255, 255);
+				pressToJoinGO.SetActive(true);
+				charClassGO.SetActive(false);
+				arrowNavGO.SetActive(false);
+				readyGO.SetActive(false);
 				break;
 			case 2:
+				isConnected = true;
 				gamepadIcon.color = new Color32(255, 255, 255, 0);
 				pressToJoinGO.SetActive(false);
-
 				charClassGO.SetActive(true);
 				arrowNavGO.SetActive(true);
+				readyGO.SetActive(false);
 				break;
 			case 3:
+				isConnected = true;
+				gamepadIcon.color = new Color32(255, 255, 255, 0);
+				pressToJoinGO.SetActive(false);
+				charClassGO.SetActive(true);
+				arrowNavGO.SetActive(false);
+				readyGO.SetActive(true);
 				break;
 		}
 	}
