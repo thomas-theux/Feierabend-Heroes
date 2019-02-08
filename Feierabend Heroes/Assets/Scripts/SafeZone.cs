d3a5a987a1bdb930a6c27b219d5438ccea085049
Thomas Theux <t.grikschas@gmail.com>
2019-02-08 14:53:07 +0100

    Implemented safe zone decreaser

--- Feierabend Heroes/Assets/Scripts/SafeZone.cs
+++ Feierabend Heroes/Assets/Scripts/SafeZone.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 public class SafeZone : MonoBehaviour {
 
-	private float decreaseSpeed = 0.02f;
-	private float minSize = 30.0f;
+	public SphereCollider safeZoneCol;
+	private Light safeZoneLight;
+
+	private float decreaseDuration = 10.0f;
+
+	private float startSizeCol = 110.0f;
+	private float endSizeCol = 7.0f;
+
+	private float startSizeLight = 140.0f;
+	private float endSizeLight = 20.0f;
 
 
 	private void Update() {
-		if (transform.localScale.x > minSize) {
-			transform.localScale -= new Vector3(decreaseSpeed, 0, decreaseSpeed);
-		}
+		safeZoneCol.radius = Mathf.MoveTowards(startSizeCol, endSizeCol, decreaseDuration);
+		safeZoneLight.spotAngle = Mathf.MoveTowards(startSizeLight, endSizeLight, decreaseDuration);
+
+		// if (transform.localScale.x > minSizeCol) {
+		// 	colliderGO.transform.localScale -= new Vector3(decreaseSpeedCol, 0, decreaseSpeedCol);
+		// 	safeZoneLight.spotAngle -= decreaseSpeedLight;
+		// }
 	}
 
 }
