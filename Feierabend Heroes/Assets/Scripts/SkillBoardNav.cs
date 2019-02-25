29cd12c4831b2215448f0633fb4026eabb7da5dc
Thomas Theux <t.grikschas@gmail.com>
2019-02-25 17:38:26 +0100

    Worked on skillboard

--- Feierabend Heroes/Assets/Scripts/SkillBoardNav.cs
+++ Feierabend Heroes/Assets/Scripts/SkillBoardNav.cs
@@ -6,54 +6,65 @@ using Rewired;
 
 public class SkillBoardNav : MonoBehaviour {
 
-    private int charID = 0;
-    private int currentIndex = 0;
+      private CharacterSheet characterSheetScript;
 
-    public List<GameObject> skillArray;
-    public Image cursorImage;
+      private int charID = 0;
+      private int currentIndex = 0;
+
+      public List<GameObject> skillArray;
+      public Image cursorImage;
 
 	private float minThreshold = 0.5f;
 	private float maxThreshold = 0.5f;
 	private bool axisXActive;
 	private bool axisYActive;
 
-    private List<Hashtable> skillData = new List<Hashtable>();
-    private Hashtable healthSkillDic = new Hashtable();
-    private Hashtable damageSkillDic = new Hashtable();
-
-
-    private void Awake() {
-        // charID = this.gameObject.transform.parent.GetComponent<CharacterMovement>().playerID;
-
-        healthSkillDic.Add("Title", "Health");
-        healthSkillDic.Add("Text", "+10%");
-        healthSkillDic.Add("Cost", new int[] {1, 1, 2, 2, 3});
-        healthSkillDic.Add("Cap", 5);
-
-        damageSkillDic.Add("Title", "Damage");
-        damageSkillDic.Add("Text", "+14");
-        damageSkillDic.Add("Cost", new int[] {1, 1, 2, 2, 3});
-        damageSkillDic.Add("Cap", 5);
-
-        skillData.Add(healthSkillDic);
-        skillData.Add(damageSkillDic);
-
-        // print((string)skillData[0]["Title"]);
-        // print((string)skillData[0]["Text"]);
-        // print((int)skillData[0]["Cost"]);
-        // print((int)skillData[0]["Cap"]);
-
-    }
-
-
-    private void Start() {
-        print((int)healthSkillDic["Cap"]);
-    }
-
-
-    private void Update() {
-        GetInput();
-    }
+      private List<Hashtable> skillData = new List<Hashtable>();
+      private Hashtable healthSkillDict = new Hashtable();
+      private Hashtable damageSkillDict = new Hashtable();
+      private Hashtable defenseSkillDict = new Hashtable();
+      private Hashtable dodgeSkillDict = new Hashtable();
+      private Hashtable critSkillDict = new Hashtable();
+      
+      private void Awake() {
+          // charID = this.gameObject.transform.parent.GetComponent<CharacterMovement>().playerID;
+          
+          // Tier one skills and stats
+          healthSkillDict.Add("Title", "Health");
+          healthSkillDict.Add("Text", "+10%");
+          healthSkillDict.Add("Cost", new int[] {1, 1, 2, 2, 3});
+          healthSkillDict.Add("Cap", 5);
+          
+          damageSkillDict.Add("Title", "Damage");
+          damageSkillDict.Add("Text", "+14");
+          damageSkillDict.Add("Cost", new int[] {1, 1, 2, 2, 3});
+          damageSkillDict.Add("Cap", 5);
+          
+          damageSkillDict.Add("Title", "Defense");
+          damageSkillDict.Add("Text", "+20%");
+          damageSkillDict.Add("Cost", new int[] {1, 1, 2, 2, 3});
+          damageSkillDict.Add("Cap", 5);
+          
+          damageSkillDict.Add("Title", "Dodge");
+          damageSkillDict.Add("Text", "+5%");
+          damageSkillDict.Add("Cost", new int[] {1, 1, 2, 2, 3});
+          damageSkillDict.Add("Cap", 5);
+          
+          damageSkillDict.Add("Title", "Critical Hit");
+          damageSkillDict.Add("Text", "+5%");
+          damageSkillDict.Add("Cost", new int[] {1, 1, 2, 2, 3});
+          damageSkillDict.Add("Cap", 5);
+          
+          skillData.Add(healthSkillDict);
+          skillData.Add(damageSkillDict);
+          skillData.Add(defenseSkillDict);
+          skillData.Add(dodgeSkillDict);
+          skillData.Add(critSkillDict);
+      }
+      
+      private void Update() {
+            GetInput();
+      }
 
 
     private void GetInput() {
